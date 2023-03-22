ebcc0208ef33109ba8e76bcf0c096a1247775e89
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2023-03-22 14:51:26 +0700

    init

---
+++ greeting.go
@@ -0,0 +1,15 @@
+package greeting
+
+import "fmt"
+
+func greeting(name string) string {
+	return "Hello, " + name
+}
+
+func greetingWithAge(name string, age int) string {
+	return fmt.Sprintf("Hello, %s. You are %d years old.", name, age)
+}
+
+func greetingWithAgeAndDrink(name string, age int, drink string) string {
+	return fmt.Sprintf("Hello, %s. You are %d years old. and your favorite drink is %s", name, age, drink)
+}
---
+++ greeting_test.go
@@ -0,0 +1,36 @@
+package greeting
+
+import "testing"
+
+func TestGreeting(t *testing.T) {
+	given := "Pokpitch"
+	want := "Hello, Pokpitch"
+
+	result := greeting(given)
+	if result != want {
+		t.Errorf("greeting(%q) = %q, want %q", given, result, want)
+	}
+}
+
+func TestGreetingWithAge(t *testing.T) {
+	givenName := "Pokpitch"
+	givenAge := 39
+	want := "Hello, Pokpitch. You are 39 years old."
+
+	result := greetingWithAge(givenName, givenAge)
+	if result != want {
+		t.Errorf("greetingWithAge(%q, %d) = %q, want %q", givenName, givenAge, result, want)
+	}
+}
+
+func TestGreetingWithAgeAndDrink(t *testing.T) {
+	givenName := "Pokpitch"
+	givenAge := 39
+	givenDrink := "Coke"
+	want := "Hello, Pokpitch. You are 39 years old. and your favorite drink is Coke"
+
+	result := greetingWithAgeAndDrink(givenName, givenAge, givenDrink)
+	if result != want {
+		t.Errorf("greetingWithAgeAndDrink(%q, %d, %q) = %q, want %q", givenName, givenAge, givenDrink, result, want)
+	}
+}
